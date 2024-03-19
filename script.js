c05f4565a48e77f1d393119f1f55a615fffe29f3
Lidia Castellanos Covarrubias <105387060+lidia-castellanos@users.noreply.github.com>
2024-03-19 15:57:59 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let botonEnviar = document.getElementById("enviar");
+const errorFaltantes=document.getElementById("mensajeErrorFaltantes");
 
 botonEnviar.addEventListener("click", function () {
 
   validarNoControl();
+  calcularEdad();
 });
 
 function validarNoControl() {
@@ -20,10 +22,52 @@ function validarNoControl() {
     valorNoControl.style.borderColor = "red";
   }
   else {
-    document.getElementById("mensajeError").innerText="";
-    
-    console.log("correcto");
-
+    valorNoControl.style.borderColor = "green";
+    document.getElementById("mensajeError").innerText = "";
   }
 
 }
+
+function calcularEdad() {
+  let edad = 0;
+
+  const fechaArray = document.getElementById("fecha").value.split("-");
+  const fechaNacimiento = document.getElementById("fecha");
+  const fechaHoy = new Date().toJSON().slice(0, 10).split("-");
+  let edadLabel = document.getElementById("edad");
+  let mesHoy = parseInt(fechaHoy[1]);
+  let mesNacimiento = parseInt(fechaArray[1]);
+  let diaHoy = parseInt(fechaHoy[2]);
+  let diaNacimiento = parseInt(fechaArray[2]);
+  let anioHoy = parseInt(fechaHoy[0]);
+  let anioNacimiento = parseInt(fechaArray[0]);
+
+  if(fechaNacimiento.value===""){
+    fechaNacimiento.style.borderColor = "red";
+    errorFaltantes.innerText="Ingresa los campos faltantes";
+
+  }
+  else{
+  if (mesHoy < mesNacimiento || (mesHoy === mesNacimiento && diaHoy < diaNacimiento)){
+    edad = (anioHoy - anioNacimiento) - 1;
+    edadLabel.value = edad;
+    fechaNacimiento.style.borderColor = "green";
+  }
+  else{
+
+    edad = anioHoy - anioNacimiento;
+    edadLabel.value = edad;
+    fechaNacimiento.style.borderColor = "green";
+  }
+  }
+  
+
+
+}
+
+let input = document.getElementById("fecha");
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Tab") {
+   calcularEdad();
+  } 
+});
